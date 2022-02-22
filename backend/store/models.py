0dd80385044bcf5dc1aaf34fe7e52347c620e3d6
maxcore25 <mkorobov2002@mail.ru>
2022-02-22 19:31:30 +0300

    Category Model written

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    """ Category Table implimented with MPTT """
+
+    name = models.CharField(
+        verbose_name=_('Category Name'),
+        help_text=_('Required and unique'),
+        max_length=255,
+        unique=True
+    )
+    slug = models.SlugField(verbose_name=_('Category safe URL'), max_length=255, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    is_active = models.BooleanField(default=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
