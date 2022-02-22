3fe0c10faa472f68099efcd5641d9ea6e4a72875
maxcore25 <mkorobov2002@mail.ru>
2022-02-22 19:59:24 +0300

    Product Model written

--- backend/store/models.py
+++ backend/store/models.py
@@ -63,3 +63,59 @@ class ProductSpecification(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    """
+    The Product table containing all product items.
+    """
+
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    title = models.CharField(
+        verbose_name=_("title"),
+        help_text=_("Required"),
+        max_length=255,
+    )
+    description = models.TextField(verbose_name=_("description"), help_text=_("Not Required"), blank=True)
+    slug = models.SlugField(max_length=255)
+    regular_price = models.DecimalField(
+        verbose_name=_("Regular price"),
+        help_text=_("Maximum 999.99"),
+        error_messages={
+            "name": {
+                "max_length": _("The price must be between 0 and 999.99."),
+            },
+        },
+        max_digits=5,
+        decimal_places=2,
+    )
+    discount_price = models.DecimalField(
+        verbose_name=_("Discount price"),
+        help_text=_("Maximum 999.99"),
+        error_messages={
+            "name": {
+                "max_length": _("The price must be between 0 and 999.99."),
+            },
+        },
+        max_digits=5,
+        decimal_places=2,
+    )
+    is_active = models.BooleanField(
+        verbose_name=_("Product visibility"),
+        help_text=_("Change product visibility"),
+        default=True,
+    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    def get_absolute_url(self):
+        return reverse("store:product_detail", args=[self.slug])
+
+    def __str__(self):
+        return self.title
