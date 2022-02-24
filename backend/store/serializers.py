ec53342ff74e68a6af90c6cbbd51af174df5c4df
maxcore25 <mkorobov2002@mail.ru>
2022-02-24 15:03:43 +0300

    Product Image Serializer added

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ProductImage
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['name', 'slug', ]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_image = ImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'regular_price']
+        fields = ['id', 'title', 'description', 'regular_price', ]
